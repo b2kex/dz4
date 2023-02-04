5295cb8d0133245e28fe54cc6acefd8e22c9d37e
b2k_ex <btaalaev@gmail.com>
2023-02-04 17:45:04 +0600

    first commit

---
+++ main.js
@@ -0,0 +1,9 @@
+function cont(count) {
+let min = document.querySelector('.minus');
+let pl = document.querySelector('.plus');
+let number = document.querySelector('.number');
+let numberValue = parseFloat(number.Value, 10);
+
+min.addEventListener('click', () => {console.log(input.value - 1)} );
+pl.addEventListener('click', () => {console.log(number + 1)});
+}
